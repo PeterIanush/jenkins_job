85fc7c3d1313a875b9e615c5c51c2ca31034e70f
Petro Yanush <pyanush@macmini0192.ddns.softservecom.com>
2019-04-24 15:26:09 +0300

    First bugs

--- jenkins/build-bootstrap.sh
+++ jenkins/build-bootstrap.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 # Script bundles puppet modules and bootstrap scripts into RPM package.
 # It's meant to be triggered from Jenkins job.
 # Expected environment variables: JENKINS_HOME, WORKSPACE, RELEASE, GIT_BRANCH, BUILD_NUMBER.
 
 set -exu
 
-echo $JENKINS_HOME >> ./logs/JENKINS_HOME.txt
-echo $WORKSPACE >> ./logs/WORKSPACE.txt
-echo $RELEASE >> ./logs/RELEASE.txt
-echo $GIT_BRANCH >> ./logs/GIT_BRANCH.txt
-echo $BUILD_NUMBER >> ./logs/BUILD_NUMBER.txt
+echo $JENKINS_HOME > ./logs/JENKINS_HOME.txt
+echo $WORKSPACE > ./logs/WORKSPACE.txt
+echo $RELEASE > ./logs/RELEASE.txt
+echo $GIT_BRANCH > ./logs/GIT_BRANCH.txt
+echo $BUILD_NUMBER > ./logs/BUILD_NUMBER.txt
 
