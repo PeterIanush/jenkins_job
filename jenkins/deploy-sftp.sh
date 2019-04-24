b09729c33a49d5f320941bba53a0c25f068312ae
Petro Yanush <pyanush@macmini0192.ddns.softservecom.com>
2019-04-24 17:09:26 +0300

    First bugs

--- jenkins/deploy-sftp.sh
+++ jenkins/deploy-sftp.sh
@@ -3,14 +3,15 @@
 # It's meant to be triggered from Jenkins job.
 # Expected environment variables: JENKINS_HOME, WORKSPACE, BOOTSTRAP_NAME, ACTION, REGION, ENVIRONMENT, STACK_NAME, CLUSTER.
 
-# set -exu
+set -exu
 
-echo $JENKINS_HOME >> JENKINS_HOME.txt
-echo $WORKSPACE >> WORKSPACE.txt
-echo $BOOTSTRAP_NAME >> BOOTSTRAP_NAME.txt
-echo $ACTION >> ACTION.txt
-echo $REGION >> REGION.txt
-echo $ENVIRONMENT >> ENVIRONMENT.txt
-echo $STACK_NAME >> STACK_NAME.txt
-echo $CLUSTER >> CLUSTER.txt
+
+echo '$JENKINS_HOME' >> JENKINS_HOME.txt
+echo '$WORKSPACE' >> WORKSPACE.txt
+echo '$BOOTSTRAP_NAME' >> BOOTSTRAP_NAME.txt
+echo '$ACTION'>> ACTION.txt
+echo '$REGION' >> REGION.txt
+echo '$ENVIRONMENT' >> ENVIRONMENT.txt
+echo '$STACK_NAME' >> STACK_NAME.txt
+echo '$CLUSTER' >> CLUSTER.txt
 
